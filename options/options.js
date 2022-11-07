559e64a0354c46ebfad8bcb1563acda9faeffee8
Crowdbotics <team@crowdbotics.com>
2022-11-07 17:28:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "snapcab_lead_submis_37512",
+  url: "https://snapcab_lead_submis_37512.botics.co",
+  api: "https://snapcab_lead_submis_37512.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
